61084b90e291ccb4db595a5857e712ce10e136d3
Alex H <53427235+Ahureau@users.noreply.github.com>
2024-03-26 09:51:40 +0100

    Black and white works with button to change grid.

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const gridContainer = document.getElementsByClassName("gridContainer")[0];
+const body = document.querySelector("body");
 
 // Create grid
 
-const squareCount = 16;
+let squareCount = 32;
 
 function createRows() {
-    for (i = 0; i < squareCount; i++) {
+    for (let i = 0; i < squareCount; i++) {
         div = document.createElement("div");
         div.classList.add("row");
         gridContainer.appendChild(div);
@@ -15,7 +16,7 @@ function createRows() {
 function createBoxes() {
     const rows = Array.from(document.getElementsByClassName("row"));
     rows.forEach((row) => {
-            for (i = 0; i < squareCount; i++) {
+            for (let i = 0; i < squareCount; i++) {
             const box = document.createElement("div");
             box.classList.add("box");
             row.appendChild(box);
@@ -37,3 +38,58 @@ gridContainer.addEventListener("mouseover", (b) => {
         b.target.classList.toggle("black");
     }
 })
+
+// Add selection of grid size
+
+function createSelector() {
+    const inputContainer = document.createElement("div");
+    inputContainer.id = "input"
+    const inputText = document.createElement("input");
+    inputText.type = "text";
+    inputText.id = "inputText";
+    const inputLabel = document.createElement("label");
+    inputLabel.textContent = "Change the grid's pixel count (max 100)";
+    inputLabel.htmlFor = "inputText"
+    const inputButton = document.createElement("button");
+    inputButton.textContent = "Update grid"
+    inputContainer.appendChild(inputLabel);
+    inputContainer.appendChild(inputText);
+    inputContainer.appendChild(inputButton);
+    body.appendChild(inputContainer);
+    updateFunction();
+}
+
+createSelector();
+
+// Runs all functions to reset the grid and create a new one.
+
+function updateFunction() {
+    const btn = document.querySelector("button");
+    btn.addEventListener("click", () => {
+        let selectionInt = parseInt(document.getElementById("inputText").value);
+        let selection = document.getElementById("inputText")
+        if (Number.isInteger(selectionInt) && selectionInt <= 100 && selectionInt > 0) {
+            squareCount = selectionInt;
+            selection.value = "";
+            resetFunction();
+            createGrid();
+        }
+    })
+}
+
+function resetFunction() {
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
+}
+
+// Color randomizer
+
+function getRandomColor() {
+    let letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
